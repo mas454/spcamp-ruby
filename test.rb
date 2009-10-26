952edd84cacc7e92aa7c4ae6ff3319a662ba9075
mas454 <masahiro454@gmail.com>
2009-10-26 20:37:39 +0900

    演算子切り替えとりあえず動作可能なもの完成

--- test.rb
+++ test.rb
@@ -1,24 +1,9 @@
-x=10
-def test(x, y)
-  x + y
-end
-def test2(x, y)
- x - y
-end
-ialias test
-ialias test2
+class Fixnum
+   def test(y)
+     self + y
+   end
+ end
+ialias test +
 
-10 test2 20
-10 test 20
-match [[10,[15,[20]]],10,40,"abc"]
- when [[10,[15, [20]]],x, k,"abc"]
-  puts k
-  #puts y
-  puts x
- when [x,20,*xs]
-  puts x
-  puts xs
-end
-
-#puts RubyVM::InstructionSequence.compile_file(__FILE__).disasm
-
+#20 test2 x
+puts 10 test 20 * 3
